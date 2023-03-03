cc8900b3804f529a22a29132f8f29760e391cab9
Arthur <peraarthur2018@gmail.com>
2023-03-03 13:17:37 -0300

    add sass and + styles global

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Roboto:wght@700;900&display=swap" rel="stylesheet" />
+        <title>ignite-news</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
